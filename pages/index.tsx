b886f850022c083c4d27ce9a41894a332bf5e1fd
Joel <itsjoelpm@gmail.com>
2023-02-06 21:25:45 +0300

    chore: complete mobile

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Nav from '../components/nav';
 import type { NextPage } from 'next';
 import { Space_Grotesk } from '@next/font/google';
 import { AiOutlineSend } from 'react-icons/ai';
+import { FiRotateCcw } from 'react-icons/fi';
 import { RiQuillPenLine } from 'react-icons/ri';
 import { MainWrapper } from '../components/common';
 import AllProjects from '../components/cards/allprojects';
@@ -83,9 +84,7 @@ const Home: NextPage = () => {
         </Styled.ArweaveSection>
 
         <Styled.AllProjects>
-          <h2>
-            All <Styled.SpecialText>Projects</Styled.SpecialText> in One Place
-          </h2>
+          <h2>All Projects in One Place</h2>
           <p>
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid
             laudantium possimus
@@ -133,45 +132,30 @@ const Home: NextPage = () => {
               className='scroll-logo'
             />
 
-            <Steps />
-            <div>
-              <p className='step-number'>01</p>
-              <div>
-                <p>Download the app</p>
-                <p>
-                  Join our 22,000 users and start managing your money with ease
-                  today.
-                </p>
-              </div>
-            </div>
-
-            <div>
-              <p>02</p>
-              <div>
-                <p>Verification</p>
-                <p>
-                  Join our 22,000 users and start managing your money with ease
-                  today.
-                </p>
-              </div>
-            </div>
-
-            <div>
-              <p>03</p>
-              <div>
-                <p>Launch 🚀</p>
-                <p>
-                  Join our 22,000 users and start managing your money with ease
-                  today.
-                </p>
-              </div>
-            </div>
+            <Steps
+              heading='Download'
+              text='Join our 22,000 users and start managing your money with ease today.'
+              num={1}
+            />
+            <Steps
+              heading='Verify'
+              text='Join our 22,000 users and start managing your money with ease today.'
+              num={2}
+            />
+            <Steps
+              heading='Launch'
+              text='Join our 22,000 users and start managing your money with ease today.'
+              num={3}
+            />
           </Styled.ProjectSteps>
 
-          <section>
-            <div>
+          <Styled.FeedbackSection>
+            <div className='launch'>
               <h3>
-                Share thoughts. <span>Get Feedback</span>
+                Get Feedback
+                <span>
+                  Re-Launch 🚀
+                </span>
               </h3>
               <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
@@ -180,38 +164,134 @@ const Home: NextPage = () => {
               </p>
             </div>
 
-            <div>
-              <Image
-                src='/scroll.svg'
-                alt='main preview'
-                width={30}
-                height={30}
-                className='scroll-logo'
-              />
-            </div>
-          </section>
+            <Image
+              src='/feedback.svg'
+              alt='main preview'
+              width={30}
+              height={30}
+            />
+          </Styled.FeedbackSection>
 
-          <section>
-            <div>
-              <h3>Builders Profile</h3>
+          <Styled.FeedbackSection>
+            <div className='launch'>
+              <h3>Build Your Profile</h3>
               <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Veritatis aut, nihil adipisci tenetur perferendis at inventore
                 sapiente deleniti dolor expedita?
               </p>
             </div>
-            <div>
+
+            <Image
+              src='/profile.svg'
+              alt='main preview'
+              width={30}
+              height={30}
+              className='profile-preview'
+            />
+          </Styled.FeedbackSection>
+        </Styled.AddProject>
+
+        <Styled.CommunitySection>
+          <h3>Become Part of A Vibrant Community</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur. Faucibus id molestie velit
+            duis vestibulum ac.
+          </p>
+
+          <div>
+            <div className='comm-item'>
+              <div className='comm-item-info'>
+                <h3>Arweave TownSquare</h3>
+                <p>
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  ipsum dolor sit amet consectetur adipisicing elit.
+                </p>
+              </div>
+
               <Image
-                src='/scroll.svg'
+                src='/tsquare.svg'
                 alt='main preview'
                 width={30}
                 height={30}
-                className='scroll-logo'
+                className='profile-preview'
               />
             </div>
-          </section>
-        </Styled.AddProject>
+
+            <div className='comm-item'>
+              <div className='comm-item-info'>
+                <h3>
+                  <span>Events</span>
+                  <span>Meetups</span>
+                  <span>Hackathons</span>
+                </h3>
+                <p>
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  Maxime aspernatur sunt blanditiis, voluptates nulla natus
+                  totam numquam. Alias, quisquam qui?
+                </p>
+              </div>
+
+              <Image
+                src='/events.svg'
+                alt='main preview'
+                width={30}
+                height={30}
+                className='profile-preview'
+              />
+            </div>
+          </div>
+        </Styled.CommunitySection>
+
+        <Styled.JobSection>
+          <h3>
+            Find your next <Styled.SpecialText>opportunity </Styled.SpecialText>
+            in Arweave
+          </h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem
+            molestias quibusdam quae cupiditate cumque. Quam!
+          </p>
+
+          <Image
+            src='/jobs.svg'
+            alt='main preview'
+            width={30}
+            height={30}
+            className='profile-preview'
+          />
+        </Styled.JobSection>
+
+        <Styled.GetStartedSection>
+          <div>
+            <h3>Get Started With WeaveHunt</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad labore
+              earum accusamus sed ducimus adipisci?
+            </p>
+          </div>
+
+          <Styled.CTA>
+            <input
+              type='text'
+              placeholder='Enter Email to Join Waitlist'
+              className={spaceGrotesk.className}
+            />
+            <button>Subscribe</button>
+          </Styled.CTA>
+        </Styled.GetStartedSection>
       </MainWrapper>
+
+      <Styled.Footer>
+        <Image
+          src='/logo-black.svg'
+          alt='main preview'
+          width={35}
+          height={35}
+        />
+
+        <p>© WeaveHunt 2023. All rights reserved</p>
+      </Styled.Footer>
     </div>
   );
 };
