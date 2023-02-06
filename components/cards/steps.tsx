11d408cb6e919139c7179168ef81ba2db7f4e545
Joel <itsjoelpm@gmail.com>
2023-02-06 21:25:07 +0300

    chore: steps cards

--- components/cards/steps.tsx
+++ components/cards/steps.tsx
@@ -1,11 +1,17 @@
 import styled from 'styled-components';
 
-const Wrapper = styled.div`
-  background: #eaf8f8;
+interface StepProps {
+  num: 1 | 2 | 3;
+}
+
+const Wrapper = styled.div<StepProps>`
+  background: ${(props) =>
+    props.num === 1 ? '#daf9f9' : props.num === 2 ? '#e2f1fd' : '#fdf1dd'};
   border-radius: 20px;
   margin: 1rem;
-  padding: 0.5rem;
+  padding: 0.8rem 0.5rem;
   display: flex;
+  gap: 0.5rem;
   align-items: center;
   justify-content: space-evenly;
 
@@ -20,26 +26,39 @@ const Wrapper = styled.div`
     align-items: center;
     justify-content: center;
     color: #2ebdb6;
+    font-weight: 500;
   }
 
-  div {
-    /* background: purple; */
+  > div {
     width: 80%;
+
+    .heading {
+      font-weight: 450;
+      font-size: 1.25rem;
+    }
+
+    .text {
+      font-weight: 300;
+      font-size: 14px;
+    }
   }
 `;
 
-const Steps = () => {
+const Steps = (props: Props) => {
   return (
-    <Wrapper>
-      <p className='step-number'>01</p>
+    <Wrapper num={props.num}>
+      <p className='step-number'>0{props.num}</p>
       <div>
-        <p>Download the app</p>
-        <p>
-          Join our 22,000 users and start managing your money with ease today.
-        </p>
+        <p className='heading'>{props.heading}</p>
+        <p className='text'>{props.text}</p>
       </div>
     </Wrapper>
   );
 };
 
+interface Props {
+  heading: string;
+  text: string;
+  num: 1 | 2 | 3;
+}
 export default Steps;
